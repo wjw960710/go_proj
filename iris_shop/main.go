53523d7f836787e15b53ae375b2cfffb3f19c68f
wujunwei <wjw960710@gmail.com>
2026-04-25 15:06:48 +0800

    清空範例代碼

--- iris_shop/main.go
+++ iris_shop/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"iris_shop/web/controllers"
-
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/mvc"
 )
 
 func main() {
 	app := iris.New()
 	app.Logger().SetLevel("debug")
 	app.RegisterView(iris.HTML("./web/views", ".html").Reload(true))
-	mvc.New(app.Party("/")).Handle(new(controllers.MovieController))
 	app.Listen(":8080")
 }
